1b24e52e4901ba163aa790613feca516c29ed804
Nicholas Musco <nmusco@Nicholass-MBP-2.intakedesk.com>
2020-09-11 17:15:54 -0400

    Adds the ability to get a list of webhooks for a specified account

--- lib/Api/WebhooksApi.php
+++ lib/Api/WebhooksApi.php
@@ -38,7 +38,6 @@ use Nmusco\AssureSign\v3\ApiException;
 use Nmusco\AssureSign\v3\Configuration;
 use Nmusco\AssureSign\v3\HeaderSelector;
 use Nmusco\AssureSign\v3\ObjectSerializer;
-use Throwable;
 
 /**
  * WebhooksApi Class Doc Comment
@@ -207,7 +206,7 @@ class WebhooksApi
                 $response->getStatusCode(),
                 $response->getHeaders()
             ];
-        } catch (Throwable $e) {
+        } catch (throwable $e) {
             $GLOBALS['log']->fatal($e->getMessage());
             throw $e;
         } catch (ApiException $e) {
@@ -374,8 +373,6 @@ class WebhooksApi
             }
         }
 
-        $GLOBALS['log']->fatal("AssureSign: \r\n\r\n\r\n{$httpBody}\r\n\r\n\r\n");
-
         $defaultHeaders = [];
         if ($this->config->getUserAgent()) {
             $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
@@ -394,6 +391,273 @@ class WebhooksApi
         $query = \GuzzleHttp\Psr7\build_query($queryParams);
         return new Request(
             'POST',
+            $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
+            $headers,
+            $httpBody
+        );
+    }
+
+    /**
+     * Operation getWebhooksList
+     *
+     * List Webhooks
+     *
+     * @param  string $x_as_user_context {username}:{DocumentNOW Account Context Identifier} (optional)
+     *
+     * @throws \Nmusco\AssureSign\v3\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return \Nmusco\AssureSign\v3\Model\ListWebhooksResponse
+     */
+    public function getWebhooksList($x_as_user_context = null)
+    {
+        list($response) = $this->getWebhooksListWithHttpInfo($x_as_user_context);
+        return $response;
+    }
+
+    /**
+     * Operation getWebhooksListWithHttpInfo
+     *
+     * List Webhooks
+     *
+     * @param  string $x_as_user_context {username}:{DocumentNOW Account Context Identifier} (optional)
+     *
+     * @throws \Nmusco\AssureSign\v3\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return array of \Nmusco\AssureSign\v3\Model\ListWebhooksResponse, HTTP status code, HTTP response headers (array of strings)
+     */
+    public function getWebhooksListWithHttpInfo($x_as_user_context = null)
+    {
+        $request = $this->getWebhooksListRequest($x_as_user_context);
+
+        try {
+            $options = $this->createHttpClientOption();
+            try {
+                $response = $this->client->send($request, $options);
+            } catch (RequestException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    $e->getCode(),
+                    $e->getResponse() ? $e->getResponse()->getHeaders() : null,
+                    $e->getResponse() ? (string) $e->getResponse()->getBody() : null
+                );
+            }
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode < 200 || $statusCode > 299) {
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        $request->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    $response->getBody()
+                );
+            }
+
+            $responseBody = $response->getBody();
+            switch($statusCode) {
+                case 200:
+                    if ('\Nmusco\AssureSign\v3\Model\ListWebhooksResponse' === '\SplFileObject') {
+                        $content = $responseBody; //stream goes to serializer
+                    } else {
+                        $content = (string) $responseBody;
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, '\Nmusco\AssureSign\v3\Model\ListWebhooksResponse', []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+            }
+
+            $returnType = '\Nmusco\AssureSign\v3\Model\ListWebhooksResponse';
+            $responseBody = $response->getBody();
+            if ($returnType === '\SplFileObject') {
+                $content = $responseBody; //stream goes to serializer
+            } else {
+                $content = (string) $responseBody;
+            }
+
+            return [
+                ObjectSerializer::deserialize($content, $returnType, []),
+                $response->getStatusCode(),
+                $response->getHeaders()
+            ];
+
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 200:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Nmusco\AssureSign\v3\Model\ListWebhooksResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+            }
+            throw $e;
+        }
+    }
+
+    /**
+     * Operation getWebhooksListAsync
+     *
+     * List Webhooks
+     *
+     * @param  string $x_as_user_context {username}:{DocumentNOW Account Context Identifier} (optional)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function getWebhooksListAsync($x_as_user_context = null)
+    {
+        return $this->getWebhooksListAsyncWithHttpInfo($x_as_user_context)
+            ->then(
+                function ($response) {
+                    return $response[0];
+                }
+            );
+    }
+
+    /**
+     * Operation getWebhooksListAsyncWithHttpInfo
+     *
+     * List Webhooks
+     *
+     * @param  string $x_as_user_context {username}:{DocumentNOW Account Context Identifier} (optional)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function getWebhooksListAsyncWithHttpInfo($x_as_user_context = null)
+    {
+        $returnType = '\Nmusco\AssureSign\v3\Model\ListWebhooksResponse';
+        $request = $this->getWebhooksListRequest($x_as_user_context);
+
+        return $this->client
+            ->sendAsync($request, $this->createHttpClientOption())
+            ->then(
+                function ($response) use ($returnType) {
+                    $responseBody = $response->getBody();
+                    if ($returnType === '\SplFileObject') {
+                        $content = $responseBody; //stream goes to serializer
+                    } else {
+                        $content = (string) $responseBody;
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, $returnType, []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+                },
+                function ($exception) {
+                    $response = $exception->getResponse();
+                    $statusCode = $response->getStatusCode();
+                    throw new ApiException(
+                        sprintf(
+                            '[%d] Error connecting to the API (%s)',
+                            $statusCode,
+                            $exception->getRequest()->getUri()
+                        ),
+                        $statusCode,
+                        $response->getHeaders(),
+                        $response->getBody()
+                    );
+                }
+            );
+    }
+
+    /**
+     * Create request for operation 'getWebhooksList'
+     *
+     * @param  string $x_as_user_context {username}:{DocumentNOW Account Context Identifier} (optional)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Psr7\Request
+     */
+    protected function getWebhooksListRequest($x_as_user_context = null)
+    {
+
+        $resourcePath = '/webHooks';
+        $formParams = [];
+        $queryParams = [];
+        $headerParams = [];
+        $httpBody = '';
+        $multipart = false;
+
+
+        // header params
+        if ($x_as_user_context !== null) {
+            $headerParams['X-AS-UserContext'] = ObjectSerializer::toHeaderValue($x_as_user_context);
+        }
+
+
+        // body params
+        $_tempBody = null;
+
+        if ($multipart) {
+            $headers = $this->headerSelector->selectHeadersForMultipart(
+                ['application/json']
+            );
+        } else {
+            $headers = $this->headerSelector->selectHeaders(
+                ['application/json'],
+                []
+            );
+        }
+
+        // for model (json/xml)
+        if (isset($_tempBody)) {
+            // $_tempBody is the method argument, if present
+            if ($headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\json_encode(ObjectSerializer::sanitizeForSerialization($_tempBody));
+            } else {
+                $httpBody = $_tempBody;
+            }
+        } elseif (count($formParams) > 0) {
+            if ($multipart) {
+                $multipartContents = [];
+                foreach ($formParams as $formParamName => $formParamValue) {
+                    $multipartContents[] = [
+                        'name' => $formParamName,
+                        'contents' => $formParamValue
+                    ];
+                }
+                // for HTTP post (form)
+                $httpBody = new MultipartStream($multipartContents);
+
+            } elseif ($headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\json_encode($formParams);
+
+            } else {
+                // for HTTP post (form)
+                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+            }
+        }
+
+
+        $defaultHeaders = [];
+        if ($this->config->getUserAgent()) {
+            $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
+        }
+
+        if ($this->config->getAccessToken()) {
+            $defaultHeaders['Authorization'] = "Bearer ".$this->config->getAccessToken();
+        }
+
+        $headers = array_merge(
+            $defaultHeaders,
+            $headerParams,
+            $headers
+        );
+
+        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        return new Request(
+            'GET',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
             $headers,
             $httpBody
